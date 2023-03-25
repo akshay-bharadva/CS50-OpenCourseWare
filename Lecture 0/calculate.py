2a8092df0e704a29756991c0bf81f4c781eebd3d
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-03-26 00:57:48 +0530

    float, format and round

--- Lecture 0/calculate.py
+++ Lecture 0/calculate.py
@@ -1,8 +1,8 @@
-x = int(input("Enter value of x: "))
-y = int(input("Enter value of y: "))
+x = float(input("Enter value of x: "))
+y = float(input("Enter value of y: "))
 
-print(f"value of x + y = {x + y}")
+print(f"value of x + y = {x + y:,}")  # format
 print(f"value of x * y = {x * y}")
-print(f"value of x / y = {x / y}")
+print(f"value of x / y = {round(x / y)}")  # round
 print(f"value of x - y = {x - y}")
 print(f"value of x % y = {x % y}")
