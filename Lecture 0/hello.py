dc45a58245ab8f8e88bfc0a8620668799334c9f2
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-03-26 01:16:13 +0530

    main function

--- Lecture 0/hello.py
+++ Lecture 0/hello.py
@@ -1,7 +1,11 @@
+def main():
+    great()
+    name = input("What's your name? ")
+    great(name)
+
+
 def great(name='Developer'):
     print(f"Good Day, {name}!")
 
 
-great()
-name = input("What's your name? ")
-great(name)
+main()
