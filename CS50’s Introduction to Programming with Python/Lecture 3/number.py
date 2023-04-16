ac98b6309de6bf8ef423cdaa629326a36189209c
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-04-16 12:45:52 +0530

    dynamic propmt

--- CS50’s Introduction to Programming with Python/Lecture 3/number.py
+++ CS50’s Introduction to Programming with Python/Lecture 3/number.py
@@ -1,12 +1,13 @@
 def main():
-    x = get_int()
+    label = "What's value of x? "
+    x = get_int(label)
     print(f"x is {x}")
 
 
-def get_int():
+def get_int(label):
     while True:
         try:
-            return int(input("What's x? "))
+            return int(input(label))
         except ValueError:
             # pass silently ignore
             pass
