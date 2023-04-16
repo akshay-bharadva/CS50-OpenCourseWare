13e4783a740b0365be390729b8651bececad8593
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-04-16 12:39:34 +0530

    try-except-pass

--- CS50’s Introduction to Programming with Python/Lecture 3/number.py
+++ CS50’s Introduction to Programming with Python/Lecture 3/number.py
@@ -1,8 +1,16 @@
-while True:
-  try:
-      x = int(input("What's x? "))
-  except ValueError:
-      print("x is not integer")
-  else:
-      print(f"x is {x}")
-      break
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            # pass silently ignore
+            pass
+            # print("x is not integer")
+
+
+main()
