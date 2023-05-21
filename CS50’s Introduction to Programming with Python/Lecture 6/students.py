0ad7f5e5e5479f10e781bf56fa6daf511b5f0559
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-05-21 11:07:50 +0530

    csv DisctWriter

--- CS50’s Introduction to Programming with Python/Lecture 6/students.py
+++ CS50’s Introduction to Programming with Python/Lecture 6/students.py
@@ -1,15 +1,37 @@
 import csv
 
-students = []
+file_name = "students.csv"
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        student = {
-            "name":row["name"],
-            "study":row["study"]
-        }
-        students.append(student)
 
-for student in sorted(students, key=lambda param: param["name"]):
-    print(f"{student['name']} studied {student['study']}")
+def main():
+    # read_csv()
+    write_csv()
+
+
+def write_csv():
+    name = input("What's your name? ")
+    study = input("What you study? ")
+
+    with open(file_name, "a") as file:
+        writer = csv.DictWriter(file, fieldnames=["name", "study"])
+        writer.writerow({"name": name, "study": study})
+
+
+def read_csv():
+    students = []
+
+    with open(file_name) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            student = {
+                "name": row["name"],
+                "study": row["study"]
+            }
+            students.append(student)
+
+    for student in sorted(students, key=lambda param: param["name"]):
+        print(f"{student['name']} studied {student['study']}")
+
+
+if __name__ == "__main__":
+    main()
