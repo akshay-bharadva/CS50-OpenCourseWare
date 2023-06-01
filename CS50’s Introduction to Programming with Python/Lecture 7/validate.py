a6c42d477ccfc61925df13b2709e1d4f1f9e989e
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-06-01 23:07:47 +0530

    email regx

--- CS50’s Introduction to Programming with Python/Lecture 7/validate.py
+++ CS50’s Introduction to Programming with Python/Lecture 7/validate.py
@@ -1,8 +1,19 @@
 import re
 
+"""
+.       any char except \n
+*       0 or more reps
++       1 or more reps
+?       0 or 1 reps
+{m}     m reps
+{m,n}   m-n reps
+^       match start
+$       match end
+"""
+
 email = input("What's your email? ").strip()
 
-if re.search("@", email):
+if re.search(r"^.+@.+\.com$", email):
     print("Valid")
 else:
     print("Invalid")
