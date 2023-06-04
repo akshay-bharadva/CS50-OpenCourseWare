358f813878e81007d38a2f48cd551de45626141f
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-06-04 09:17:29 +0530

    sub level domain and top level domain

--- CS50’s Introduction to Programming with Python/Lecture 7/validate.py
+++ CS50’s Introduction to Programming with Python/Lecture 7/validate.py
@@ -27,7 +27,7 @@ A|B     A or B
 
 email = input("What's your email? ").strip()
 
-if re.search(r"^\w+@\w+\.com$", email, re.IGNORECASE):
+if re.search(r"^\w+@(\w+\.)?\w+\.com$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
