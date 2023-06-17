f56807b90bd589195b0d7dd3ab2eec4a3b2f1795
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-06-17 23:10:44 +0530

    getter and setter method

--- CS50’s Introduction to Programming with Python/Lecture 8/student.py
+++ CS50’s Introduction to Programming with Python/Lecture 8/student.py
@@ -1,16 +1,32 @@
 class Student:
     def __init__(self, name, house):  # constructor
-        if not name:
-            raise ValueError("Missing name")
-        if not house:
-            raise ValueError("Missing house")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError(f"Invalid house")
         self.name = name
         self.house = house
 
     def __str__(self):
         return f"{self.name} from {self.house}"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if not house:
+            raise ValueError("Missing house")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError(f"Invalid house")
+        self._house = house
 
 
 def main():
