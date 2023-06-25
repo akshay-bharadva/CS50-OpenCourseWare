c7284e1f390de134f06833d07d82bea97d66ce45
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-06-25 18:23:09 +0530

    doc string

--- CS50’s Introduction to Programming with Python/Lecture 9/meows.py
+++ CS50’s Introduction to Programming with Python/Lecture 9/meows.py
@@ -1,4 +1,13 @@
 def meow(n: int) -> str:
+    """
+    Meow n times
+
+    :param n: Number of times to Meow
+    :type n: int
+    :raise TypeError: If n is not int
+    :return: A str with n times meow
+    :rtype: str
+    """
     return "meow " * n
 
 
