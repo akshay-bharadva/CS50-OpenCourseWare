600ec6658ea8fb1337fe475cd1193d58b352ec94
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-06-25 10:34:36 +0530

    global variable alt

--- CS50’s Introduction to Programming with Python/Lecture 9/bank.py
+++ CS50’s Introduction to Programming with Python/Lecture 9/bank.py
@@ -1,21 +1,28 @@
-balance = 0
+class Account:
+    def __init__(self):
+        self.balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @balance.setter
+    def balance(self, value):
+        self._balance = value
+
+    def deposit(self, n):
+        self.balance += n
+
+    def withdraw(self, n):
+        self.balance -= n
 
 
 def main():
-    print(f"Balance: {balance}")
-    deposite(100)
-    withdrawn(50)
-    print(f"Balance: {balance}")
-
-
-def deposite(n):
-    global balance
-    balance += n
-
-
-def withdrawn(n):
-    global balance
-    balance -= n
+    account = Account()
+    print(f"Balance: {account.balance}")
+    account.deposit(100)
+    account.withdraw(50)
+    print(f"Balance: {account.balance}")
 
 
 if __name__ == "__main__":
