acd804aa0862eb99e1b2f4d59f7d797c22b0675a
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-06-27 22:30:39 +0530

    filter list with functional

--- CS50’s Introduction to Programming with Python/Lecture 9/filter.py
+++ CS50’s Introduction to Programming with Python/Lecture 9/filter.py
@@ -1,7 +1,14 @@
 numbers = [10, 12, 45, 34, 56, 25, 78, 45, 15, 35, 65, 20]
 
-adults = [
-    number for number in numbers if number > 18
-]
+# adults = [
+#     number for number in numbers if number > 18
+# ]
+
+
+def is_adult(age):
+    return age > 18
+
+
+adults = filter(is_adult, numbers)
 
 print("adults:", sorted(adults))
