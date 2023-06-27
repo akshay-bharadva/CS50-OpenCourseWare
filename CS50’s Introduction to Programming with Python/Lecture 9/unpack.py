e07fa9642e8fc7f9da89e8a4aad6127597a043cc
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-06-27 21:55:27 +0530

    args and kwargs

--- CS50’s Introduction to Programming with Python/Lecture 9/unpack.py
+++ CS50’s Introduction to Programming with Python/Lecture 9/unpack.py
@@ -1,3 +1,9 @@
 # * to unpack as list like *[10,20,30]
 # ** to unpack as dict like **{price:10,quantity:2,expense:30}
 # it's similar to ... operation in JS
+
+def f(*args, **kwargs):
+    print(args, kwargs)
+
+
+f(1, 2, True, "Name", theme="dark", type="landscape")
