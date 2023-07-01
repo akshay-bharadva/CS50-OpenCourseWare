693f2e3a0ad639a06f88354ddb4701ac786fb831
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-07-01 15:09:07 +0530

    dict comprehension

--- CS50’s Introduction to Programming with Python/Lecture 9/gryffindor.py
+++ CS50’s Introduction to Programming with Python/Lecture 9/gryffindor.py
@@ -1,6 +1,6 @@
 students = ["Ron", "Harry"]
 
-gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+gryffindors = {student: "Grayffindor" for student in students}
 
 # for student in students:
 #     gryffindors.append({"name": student, "house": "Gryffindor"})
