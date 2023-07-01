434bcdf64a926eac7129d5f698339a6362f47dfe
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-07-01 15:08:07 +0530

    list comprehension

--- CS50’s Introduction to Programming with Python/Lecture 9/gryffindor.py
+++ CS50’s Introduction to Programming with Python/Lecture 9/gryffindor.py
@@ -1,8 +1,8 @@
 students = ["Ron", "Harry"]
 
-gryffindors = []
+gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
 
-for student in students:
-    gryffindors.append({"name": student, "house": "Gryffindor"})
+# for student in students:
+#     gryffindors.append({"name": student, "house": "Gryffindor"})
 
 print(gryffindors)
