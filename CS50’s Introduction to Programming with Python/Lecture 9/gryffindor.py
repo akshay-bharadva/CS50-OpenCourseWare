4a152c24b08017c4ea47ba97e75479d1ce684430
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-07-01 15:12:36 +0530

    student, index enumerate

--- CS50’s Introduction to Programming with Python/Lecture 9/gryffindor.py
+++ CS50’s Introduction to Programming with Python/Lecture 9/gryffindor.py
@@ -5,7 +5,10 @@ students = ["Ron", "Harry"]
 # for student in students:
 #     gryffindors.append({"name": student, "house": "Gryffindor"})
 
-for i in range(len(students)):
-    print(i+1, students[i])
+# for i in range(len(students)):
+#     print(i+1, students[i])
+
+for i, student in enumerate(students):
+    print(i+1, student)
 
 # print(gryffindors)
